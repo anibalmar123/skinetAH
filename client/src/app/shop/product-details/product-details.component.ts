de70508daf030072360cf1b507e6a14c708e109f
Anibal Marcelo Huichulef Larrondo <anibalhl@xms.cl>
2022-07-17 22:48:37 -0400

    End of section 12

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
@@ -11,7 +12,10 @@ import { ShopService } from '../shop.service';
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute,
+    private bcService: BreadcrumbService ) {
+      this.bcService.set('@productDetails', ' ');
+    }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -19,7 +23,11 @@ export class ProductDetailsComponent implements OnInit {
 
   loadProduct() {
     this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe({
-      next: (v) => {this.product = v},
+      next: (v) => 
+      {
+        this.product = v
+        this.bcService.set('@productDetails', this.product.name)
+      },
       error: (e) => console.log(e),
       complete: () => console.info('complete individual product')
     });
